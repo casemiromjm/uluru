6cf0a886391b9daea2c540d3469c8ed32251233e
casemiromjm <casemiromelojorge@gmail.com>
2026-01-12 12:40:15 +0000

    done part 2

--- main.pl
+++ main.pl
@@ -21,7 +21,7 @@ anywhere(_, _).
 
 % verifies if the first two parameters occur consecutively in the list provided in the third parameter
 consecutive(X, Y, Board) :-
-    append(Prefix, [X, Y | Suffix], Board).
+    append(_Prefix, [X, Y | _Suffix], Board).
 
 % X is always next to itself
 next_to(X,X,_).
@@ -50,7 +50,7 @@ one_space(X, Y, [A, B, C, D, E, F]) :-
 
 % one space aux using append
 one_space_aux(X, Y, Board) :-
-    append(Prefix, [X, _mid, Y | Suffix], Board).
+    append(_Prefix, [X, _Mid, Y | _Suffix], Board).
 
 % %%%%%%%%%%%%%%%%%%%%%%
 % ACROSS
@@ -65,8 +65,8 @@ across(X, Y, Board) :-
     get_position(Y, Board, PosY),
     valid_across(PosX, PosY).
 
-% get position(+Color, +Board, -Pos)
-get_position(Color, [Color | Tail], 1).
+% get position(+Color, +Board, -Pos). Default Pos is 1 to be 1-index based
+get_position(Color, [Color | _Tail], 1).
 
 get_position(Color, [_ | Tail], Pos) :-
     get_position(Color, Tail, Pos1),
@@ -155,10 +155,55 @@ check_constraints([Con | Cons], Board) :-
     call(Con, Board),
     check_constraints(Cons, Board).
 
-% %%%%%%%%%%%%%%%%%%%%%%
-% EXEMPLOS
+% ---------------------------
+% PART 2
+% ---------------------------
 
-%% 12 solutions
+% best_score(+Constraints, -Score) that computes the best score for a list of constraints. A score of 0 corresponds to satisfying all constraints, -1 to satisfying all but one constraint, etc.
+best_score(Constraints, Score) :-
+    % findall/3 => findall(Template, Goal, List). Template: This is the specific "shape" of the data you want to collect (usually a variable like Score). Goal: This is the query you want Prolog to solve repeatedly through backtracking (e.g., "Find a board and calculate its score"). List: This is the resulting list where Prolog stores every instance of the Template that satisfied the Goal.
+    findall(S, get_scores(Constraints, S), AllScores),
+    max_list(AllScores, Score).
+
+get_scores(Constraints, Score) :-
+    colors(Colors),
+    permutation(Colors, Board),
+    count_violations(Constraints, Board, Points),
+    Score is Points*(-1).
+
+% given a List returns the biggest => max_list(List, Elem)
+max_list([X], X).
+
+max_list([H | T], Big) :-
+    max_list(T, BigTail),
+
+    ( % If H > BigTail Then
+        H > BigTail ->
+            Big is H
+        % Else
+        ;
+            Big is BigTail
+    ).
+
+% the score is (-1)*violations => count_violations(Constraints, Board, Count)
+count_violations([], _Board, 0).
+
+% constraint respected
+count_violations([C | Rest], Board, Count) :-
+    call(C, Board), !,
+    % constraint C was successful, MUST NOT backtrack (cut!)
+    count_violations(Rest, Board, Count).
+
+% failed constraint, prolog looks for another clause (this one)
+count_violations([_ | Rest], Board, Count) :-
+    count_violations(Rest, Board, Count1),
+    Count is Count1 + 1.
+
+% ---------------------------
+% EXAMPLES
+% ---------------------------
+
+%% 12 solutions => S = 0
 example(1, [ next_to(white,orange),
     next_to(black,black),
     across(yellow,orange),
@@ -166,7 +211,7 @@ example(1, [ next_to(white,orange),
     position(blue,[1,2,6]),
     across(yellow,blue) ]).
 
-%% 1 solution
+%% 1 solution => S = 0
 example(2, [ across(white,yellow),
     position(black,[1,4]),
     position(yellow,[1,5]),
@@ -174,7 +219,7 @@ example(2, [ across(white,yellow),
     same_edge(blue,yellow),
     one_space(orange,black) ]).
 
-%% no solutions (5 constraints are satisfiable)
+%% no solutions (5 constraints are satisfiable) => S = -1
 example(3, [ across(white,yellow),
     position(black,[1,4]),
     position(yellow,[1,5]),
@@ -182,14 +227,10 @@ example(3, [ across(white,yellow),
     same_edge(blue,yellow),
     one_space(orange,black) ]).
 
-%% same as above, different order of constraints
+%% same as above, different order of constraints => S = -1
 example(4, [ position(yellow,[1,5]),
     one_space(orange,black),
     same_edge(green, black),
     same_edge(blue,yellow),
     position(black,[1,4]),
     across(white,yellow) ]).
-
-% ---------------------------
-% PART 2
-% ---------------------------
